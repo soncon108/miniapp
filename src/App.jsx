a825d56fc07e013899742e981e07586f47f08eac
soncon108 <thanhson2001810@gmail.com>
2023-07-14 17:09:45 +0700

    rminiap2

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,33 @@ import {
   MenuFoldOutlined,
   MoneyCollectOutlined,
   ClockCircleOutlined,
-  UserOutlined
+  UserOutlined, 
+  FileTextOutlined
 } from "@ant-design/icons";
-import { Layout, Menu, Button, theme, Image, Space, Avatar } from "antd";
+import { Layout, Menu, Button, theme, Image, Space, Avatar, Switch } from "antd";
 import { useState } from "react";
 import HelloWorld from "./HelloWorld/Show";
 import ChessBoard from "./ChessBoard/ChessBoard";
 import Calculator from "./Calculator/Calculator";
 import ConvertMoney from "./ConvertMoney/Convert";
 import Pomodoro from "./Pomodoro/PomodoroClock";
+import Quote from "./Quote/Quote";
 import { useTranslation } from "react-i18next";
 import i18n from "i18next";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import logo from "./Images/logo.png"
+import flagUs from "./Images/flagUs.png"
+import flagVi from "./Images/flagVi.png"
+
 
 const { Header, Sider, Content, Footer } = Layout;
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   const { t } = useTranslation();
-  const changeLanguage = (e) => {
-    const languageValue = e.target.value;
-    i18n.changeLanguage(languageValue);
+  const changeLanguage = (value) => {
+    i18n.changeLanguage(value);
   };
 
   const colorBgContainer = theme.useToken("color-bg-container");
@@ -74,10 +78,15 @@ const App = () => {
                 label: <Link to={"/calculator"}>{t("calculator")}</Link>,
               },
               {
-                key: " 5",
+                key: "5",
                 icon: <ClockCircleOutlined />,
                 label: <Link to={"/pomodoro"}>{t("pomodoro")}</Link>,
               },
+              {
+                key: "6",
+                icon: <FileTextOutlined />,
+                label: <Link to={"/quote"}>{t("quote")}</Link>,
+              }
             ]}
           />
         </Sider>
@@ -105,11 +114,14 @@ const App = () => {
                   <Space style={{fontSize: 17}}>Nguyễn Thanh Sơn</Space>
                 </Space>
               </Space>
-              <Space wrap>
-                <select className="changeLanguage" onChange={changeLanguage}>
-                  <option value="eng" >English</option>
-                  <option value="vie">Vietnamese</option>
-                </select>
+
+              <Space direction="vertical">
+                <Switch 
+                checkedChildren={<img src={flagUs} value="eng" width="30px" height="20px" />} 
+                unCheckedChildren={<img src={flagVi} value="vie"  width="30px" height="18px" />} 
+                defaultChecked ={i18n.language === 'eng'}
+                onChange={(checked) => changeLanguage(checked ? 'eng' : 'vie')}
+                />
               </Space>
             </Space>
           </Header>
@@ -123,6 +135,7 @@ const App = () => {
               <Route path="/chessboard" element={<ChessBoard />} />
               <Route path="/calculator" element={<Calculator />} />
               <Route path="/pomodoro" element={<Pomodoro />} />
+              <Route path="/quote" element={<Quote />} />
             </Routes>
           </Content>
           <Footer className="footer">
--- src/HelloWorld/Show.jsx
+++ src/HelloWorld/Show.jsx
@@ -3,16 +3,30 @@ import "./Show.css";
 
 function Show() {
   const [info, setInfo] = useState("");
-  const hanldeShow = (event) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleShow = (event) => {
     setInfo(event.target.value);
   };
+
+  const handleCheck = () => {
+    setChecked(true);
+  };
+
+  const handleUncheck = () => {
+    setChecked(false);
+  };
+
+  const displayInfo = checked ? info.toUpperCase() : info.toLowerCase();
 
   return (
     <>
       <div className="containerShow">
         <h3>Nhập Là Hiển Thị</h3>
-        <input value={info} onChange={hanldeShow} />
-        <p>{info}</p>
+        <input value={info} onChange={handleShow} />
+        <p className="pShow">{displayInfo}</p>
+        <button onClick={handleCheck}>Check</button>
+        <button onClick={handleUncheck}>Uncheck</button>
       </div>
     </>
   );
